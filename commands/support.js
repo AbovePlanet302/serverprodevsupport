ec71167ac528bb3ce64d6d899a4e314d7e2927e0
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-03-29 16:06:23 -0400

    Add files via upload

---
+++ commands/support.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const red = botconfig.red;
+const green = botconfig.green;
+const orange = botconfig.orange;
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    if(args[0] == "help"){
+      message.reply("Usage: !report <user> <reason>");
+      return;
+    }
+    let rUser = message.member || message.author || message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return errors.cantfindUser(message.channel);
+    let rreason = args.join(" ").slice(1);
+    if(!rreason) return errors.noReason(message.channel);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Suport Request")
+    .setColor("#8ca4ff")
+    .addField("Ticket Creator", `${message.author}`)
+    .addField("Time Created", message.createdAt)
+    .addField("Problem", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "support");
+    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+    reportschannel.send(reportEmbed);
+
+}
+
+module.exports.help = {
+  name: "support"
+}
