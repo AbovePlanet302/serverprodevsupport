0abbcf684a6a9ffb020ba4ef65c588e29eded223
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-10-21 17:27:56 -0400

    Create staff.js

---
+++ commands/staff.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+
+const economy = require("../utils/economy.js")
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You do not have permission to do this.");
+
+
+
+  if(!message.mentions.users.first()) return message.reply("You did not mention a user.")
+
+  
+
+  economy.setCoins(message.mentions.users.first().id, "**Helper**");
+
+  message.channel.send(message.mentions.users.first().username + " is now a Helper.")
+
+  
+
+
+
+}
+
+
+
+module.exports.help = {
+
+  name: "staff"
+
+}
