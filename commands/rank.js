aad6886d75fd5dddc584439dd2595d1a56755b70
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-10-21 13:31:21 -0400

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,18 +1,26 @@
 const Discord = require("discord.js");
 let rank = require("../ranks.json");
 let coins = require("../coins.json");
+let xp = require("../xp.json")
+
 
 module.exports.run = async (bot, message, args) => {
   //!coins
   if(!rank[message.author.id]){
     rank[message.author.id] = {
       rank: "**None**",
-      coins: 0
+      coins: 0,
+      xp: 0,
+      level: 1
     };
   }
 
   let uRank = rank[message.author.id].rank;
   let uCoins = coins[message.author.id].coins;
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 300;
+  let difference = nxtLvlXp - curxp;
 
 
   let coinEmbed = new Discord.RichEmbed()
@@ -20,6 +28,8 @@ module.exports.run = async (bot, message, args) => {
   .setColor(0xffffff)
   .addField("Staff Rank", uRank )
   .addField("Coins ", uCoins)
+  .addField("Level ", curlvl, true)
+  .addField("XP ", curxp, true)
   .setFooter('Searched User') //Add a footer
   .setTimestamp() //Timestamp the footer
 
