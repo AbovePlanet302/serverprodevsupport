f2a498b268cb8cd29de78d5294712954b2aa1d3a
Aresiel <love.dittlau@gmail.com>
2019-10-22 16:24:23 +0200

    Betterified pay command

--- commands/pay.js
+++ commands/pay.js
@@ -2,79 +2,27 @@ const Discord = require("discord.js");
 
 const fs = require("fs");
 
-let coins = require("../coins.json");
-
+const economy = require('../utils/economy.js')
 
 
 module.exports.run = async (bot, message, args) => {
 
-  //!pay @isatisfied 59345
-
-
-
-  if(!coins[message.author.id]){
-
-    return message.reply("You don't have any coins!")
-
-  }
-
-
-
-  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-
-
-
-  if(!coins[pUser.id]){
-
-    coins[pUser.id] = {
-
-      coins: 0
-
-    };
-
-  }
-
-
-
-  let pCoins = coins[pUser.id].coins;
-
-  let sCoins = coins[message.author.id].coins;
-
-
-
-  if(sCoins < args[0]) return message.reply("Not enough coins there!");
-
-
-
-  coins[message.author.id] = {
-
-    coins: sCoins - parseInt(args[1])
-
-  };
-
-
-
-  coins[pUser.id] = {
-
-    coins: pCoins + parseInt(args[1])
-
-  };
-
-
-
-  message.channel.send(`${message.author} has given ${pUser} ${args[1]} coins.`);
-
-
-
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-
-    if(err) cosole.log(err)
-
-  });
+  let recipitant = message.mentions.users.first();
+  let recipitantCoins = economy.getCoins(recipitant.id)
+  let userCoins = economy.getCoins(message.author.id)
+  let amount = Math.floor(args[1]);
 
 
 
 
+
+  if(amount <= 0) return message.reply("You need to pay atleast 1 coin.")
+  if(userCoins < amount) return message.reply("You do not have enough coins.");
+  
+  economy.setCoins(recipitant.id, recipitantCoins+amount)
+  economy.setCoins(message.author.id, userCoins-amount)
+
+  message.channel.send(`<@${message.author.id}> payed <@${recipitant.id}> ${amount} coins.`)
 
 }
 
