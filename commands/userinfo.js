c6d21ef1a40fb6ba80f241416ffe858d70bdafb4
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-11-02 11:33:03 -0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,36 +1,17 @@
 const Discord = require("discord.js");
-let rank = require("../ranks.json");
-let economy = require("../utils/economy.js")
-let xp = require("../xp.json")
 
 
 module.exports.run = async (bot, message, args) => {
-  //!coins
-  if(!rank[message.author.id]){
-    rank[message.author.id] = {
-      rank: "**None**",
-      coins: 0,
-      xp: 0,
-      level: 1
     };
   }
 
-  let uRank = rank[message.author.id].rank;
-  let uCoins = economy.getCoins(message.author.id)
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvlXp = curlvl * 300;
-  let difference = nxtLvlXp - curxp;
-
-
   let coinEmbed = new Discord.RichEmbed()
-  .setTitle("User Information")
-  .setColor(0xffffff)
-  .addField("Staff Rank", uRank )
-  .addField("Coins ", uCoins)
-  .addField("Level ", curlvl, true)
-  .addField("XP ", curxp, true)
-  .setFooter('Searched User') //Add a footer
+  .setTitle("User Information: ", message.author.username)
+  .setColor(random)
+  .addField("Name: ", uRank )
+  .addField("User ID: ", message.author.id)
+  .addField("Online Status:  ", message.author.status)
+  .addField("Joined Date: ", messge.author.guild.joinedAt)
   .setTimestamp() //Timestamp the footer
 
   message.channel.send(coinEmbed);
@@ -38,5 +19,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-  name: "userinfo"
+  name: "info"
 }
